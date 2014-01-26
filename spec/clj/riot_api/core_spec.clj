2bb4ea21427fe1028e43a87c9c32b6dddc6e527e
Taka Goto <tak.yuki@gmail.com>
2014-01-26 14:44:45 -0600

    testing url generator

--- spec/clj/riot_api/core_spec.clj
+++ spec/clj/riot_api/core_spec.clj
@@ -1,2 +1,22 @@
 (ns riot-api.core-spec
-  (:require [riot-api.core   :as riot]))
+  (:require [clojure.set     :refer [union]]
+            [riot-api.core   :refer [generate-url]]
+            [speclj.core     :refer :all]))
+
+(def champions-data
+  {:api-version "v1.1"
+   :region "na"
+   :api-key nil
+   :name "taka"})
+
+(describe "champions url"
+  (with url "https://prod.api.pvp.net/api/lol/na/v1.1/champion?api_key=api-key")
+
+  (it "generates the url champions api"
+    (should= @url
+      (generate-url :champions (union champions-data {:api-key "api-key"}))))
+
+  (it "should return error if api key is nil"
+    (should= "api key must be included"
+      (generate-url :champions champions-data)))
+  )
--- src/clj/riot_api/core.clj
+++ src/clj/riot_api/core.clj
@@ -6,6 +6,11 @@
 (def url "https://prod.api.pvp.net/api/lol/")
 
 (defn generate-url [type data]
-  (if
-    (= :champions type)
-    (str url (:api-version data) "/champion?api_key=" (:api-key data))))
+  (if (nil? (:api-key data))
+    "api key must be included"
+    (if
+      (= :champions type)
+      (str
+        url (:region data) "/"
+        (:api-version data) "/champion?api_key="
+        (:api-key data)))))
